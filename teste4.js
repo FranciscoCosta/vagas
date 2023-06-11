71024f3b1273116d88648b8abf388d929355a5fe
Francisco Costa <francisco100eg@gmail.com>
2023-06-11 16:37:25 -0300

    :Fix: putUsers route

--- teste4.js
+++ teste4.js
@@ -2,12 +2,30 @@ var data =  require("./fakeData");
 
 module.exports =  function(req, res) {
   
-    var id =  req.query.id;
+    const id =  req.query.id;
+    const name =  req.body.name;
+    const job =  req.body.job;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    try {
+        const userData = data.find(user => user.id == id);
+        if (!userData) {
+            res.status(404).send("User not found");
+        }else{
+            //Aqui eu atualizo o usuário com os novos dados caso eles tenham sido enviados
+            if(name){
+                userData.name = name;
+            }
+            if(job){
+                userData.job = job;
+            }
+        }
+        //Aqui eu atualizo o usuário no array de usuários e retorno o usuário atualizado
+        data.splice(userData.id - 1, 1, userData);
+        res.status(200).send(userData);
 
-    res.send(reg);
+     } catch(error) {
+        //Aqui eu retorno o erro em caso de falha
+            res.send(error);
+        }
 
 };
