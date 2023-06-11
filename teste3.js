ae9b7481df96bd2caa84756696100d1af57b2f32
Francisco Costa <francisco100eg@gmail.com>
2023-06-11 16:29:34 -0300

    :Fix: deleteUsers route

--- teste3.js
+++ teste3.js
@@ -1,15 +1,16 @@
 var data =  require("./fakeData");
 
 module.exports = function(req, res) {
-  
-    var name =  req.query.name;
-
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+    //Devemos utilizar o id do usuário para apagar o usuário em vez do nome pois o nome não é único
+    const id =  req.query.id;
+    //Aqui eu procuro o usuário pelo nome
+    const userData = data.find(user => user.id == id)
+    if (!userData) {
+        //Aqui eu retorno o status 404 em caso de falha
+        res.status(404).send("User not found");
+    }else{
+        //Aqui eu apago o usuário do array de usuários e retorno o array atualizado
+        data.splice(userData.id - 1, 1);
+        res.status(200).send(data);
     }
-
-    res.send("success");
-
-};
+    };
