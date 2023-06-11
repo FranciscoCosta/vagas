d272db09156cc4825c4e4933938d7f208411d3d4
Francisco Costa <francisco100eg@gmail.com>
2023-06-11 16:20:34 -0300

    :Doc: Added comments to test1 file

--- teste1.js
+++ teste1.js
@@ -4,13 +4,17 @@ var data =  require("./fakeData");
 const getUser = ( req, res, next ) => {
     const name =  req.query.name;
     try {
+        //Aqui eu procuro o usuário pelo nome
         const userData = data.find(user => user.name === name);
         if (!userData) {
+            //Aqui eu retorno o status 404 em caso de falha
             res.status(404).send("User not found");
         }else{
+            //Aqui eu retorno o novo usuário em caso de sucesso
             res.status(200).send(userData);
         }
     } catch (error) {
+        //Aqui eu retorno o erro em caso de falha
         res.send(error);
     }
 };
@@ -18,8 +22,10 @@ const getUser = ( req, res, next ) => {
 const getUsers = ( req, res, next ) => {
     const users = data;
     try {
+        //Aqui eu retorno o novo usuário em caso de sucesso
         res.status(200).send(users);
     } catch (error) {
+        //Aqui eu retorno o erro em caso de falha
         res.send(error);
     }
 };
